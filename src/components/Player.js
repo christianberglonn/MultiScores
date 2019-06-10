c70a446e7cf29e0c1aa43be5cbbaa6e8c50b6e3e
christianberglonn <christianberglonn@gmail.com>
2019-06-10 10:08:02 +0200

    first commit

---
+++ src/components/Player.js
@@ -0,0 +1,51 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import Counter from './Counter';
+import Icon from './Icon';
+
+//PureComponent removes wasted renders and only renders the player when score is updated
+
+class Player extends PureComponent {
+
+    static propTypes = {
+        changeScore: PropTypes.func,
+        removePlayer: PropTypes.func,
+        name: PropTypes.string.isRequired,
+        score: PropTypes.number.isRequired,
+        id: PropTypes.number,
+        index: PropTypes.number,
+        isHighScore: PropTypes.bool
+    };
+
+    render() {
+        const {
+            name,
+            id,
+            score,
+            index,
+            removePlayer,
+            changeScore,
+            isHighScore
+        } = this.props;
+
+        return (
+            <div className="player">
+                <span className="player-name">
+                    <button className="remove-player" onClick={() => removePlayer(id)}>✖</button>
+                    <Icon isHighScore={isHighScore} />
+                    {name}
+                </span>
+
+                <Counter
+                    score={score}
+                    index={index}
+                    changeScore={changeScore}
+                />
+            </div>
+        );
+    }
+}
+
+
+
+export default Player;
